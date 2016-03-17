8b41b45235ed41ab0a89bd5e9568ca2990400512
Lujia Zhang <lujia.zhang@moovweb.com>
2016-03-16 19:33:31 -0700

    my web

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+
+  def create
+  # some object you want to create
+  # if the object.save is fine
+  #   redirect_to object
+  # else
+  #   render new with the errors
+  # end
+  end
+
+  def index
+  end
+end
